4da06df2f42c1d78ab8066093297b9642e01f419
Pushpendra Kumar Ahirwar <rajpushpendra19@gmail.com>
2024-07-16 13:23:41 +0530

    Day 4 Completed

--- Day-4/Index.js
+++ Day-4/Index.js
@@ -1,12 +1,10 @@
 //Activity-1
 //Task-1
-
 console.log("<---------Task 1 ------------>");
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
 //Task-2
-
 console.log("<---------Task 2 ------------>");
 const number = 5;
 for (let i = 1; i <= 10; i++) {
@@ -15,7 +13,6 @@ for (let i = 1; i <= 10; i++) {
 
 //Activity-2
 //Task-3
-
 console.log("<---------Task 3 ------------>");
 let sum = 0;
 let iteration = 1;
@@ -26,7 +23,6 @@ while (iteration <= 10) {
 console.log(`Total sum of from number 1-10 is: ${sum}`);
 
 //Task-4
-
 console.log("<---------Task 4 ------------>");
 let iteration1 = 10;
 while (iteration1 >= 1) {
@@ -36,7 +32,6 @@ while (iteration1 >= 1) {
 
 //Activity-3
 //Task-5
-
 console.log("<---------Task 5 ------------>");
 let iteration2 = 1;
 do {
@@ -45,7 +40,6 @@ do {
 } while (iteration2 <= 5);
 
 //Task-6
-
 console.log("<---------Task 6 ------------>");
 let factorial = 1;
 const num = 5;
@@ -58,7 +52,6 @@ console.log(`Factorial of ${num} is = ${factorial}`);
 
 //Activity -4
 //Task 7
-
 console.log("<---------Task 7 ------------>");
 for (let i = 1; i <= 5; i++) {
     for (let j = 1; j <= i; j++) {
