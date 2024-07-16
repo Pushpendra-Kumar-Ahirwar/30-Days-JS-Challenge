7a4ae3ce088f5d4f5134cab5bbd01af799b77500
Pushpendra Kumar Ahirwar <rajpushpendra19@gmail.com>
2024-07-16 13:42:42 +0530

    Day 4 Completed

--- Day-4/Index.js
+++ Day-4/Index.js
@@ -59,3 +59,20 @@ for (let i = 1; i <= 5; i++) {
     }
     console.log();
 }
+
+//Activity-5
+//Task 8
+for (let i = 1; i <= 10; i++) {
+    if (i == 5) {
+        continue;
+    }
+    console.log(i);
+}
+
+//Task 9
+for (let i = 1; i <= 10; i++) {
+    if (i == 7) {
+        break;
+    }
+    console.log(i);
+}
